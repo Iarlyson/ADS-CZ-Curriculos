622d7a39284ad7e5fb0f8d0969e3ecd182bed12c
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-12-06 20:20:09 -0300

    Banco postgres montado

--- api/src/app.js
+++ api/src/app.js
@@ -5,10 +5,7 @@ const app = express();
 
 // ==> Rotas da API:
 const index = require('./routes/index');
-const productRoute = require('./routes/product.routes');
 const userRoute = require('./routes/user.routes');
-const carrinhoRoute = require('./routes/carrinho.routes');
-const pedidosRoute = require('./routes/pedidos.routes')
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,8 +14,5 @@ app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
 app.use(index);
-app.use('/api/', productRoute);
 app.use('/api/', userRoute);
-app.use('/api/', carrinhoRoute);
-app.use('/api/', pedidosRoute);
 module.exports = app;
