75df1553a4ac9ccd590a17331a07c4bfec3f3c59
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2020-12-06 05:28:10 -0300

    Ajustes Lançados

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ADS-IFPB
+# ADS-CZ Currículos
+
+
+### Equipe:
+#### Francisco Iarlyson Santana de Andrade
+#### Maria Kelcilene Parnaiba Bispo
+
+
+
+CREATE TABLE Aluno(
+	id SERIAL,
+	Matricula VARCHAR(10),
+	Nome VARCHAR(50),
+	DataNascimento VARCHAR(10),
+	AnoConclusao VARCHAR(10),
+	TurmaReferente VARCHAR(10),
+	PRIMARY KEY(Id)
+);
+
+INSERT INTO Aluno(Matricula, Nome, DataNascimento, AnoConclusao, TurmaReferente) VALUES ('2017120101', 'Joao Carlos Brasileiro', '10-01-1199', '12-12-2019', '2017.1');
+
+INSERT INTO Aluno(Matricula, Nome, DataNascimento, AnoConclusao, TurmaReferente) VALUES('2017120102','Maria Eduarda Nascimento', '15-06-1197', '12-06-2016', '2013.2');
+
+INSERT INTO Aluno (Matricula, Nome, DataNascimento, AnoConclusao, TurmaReferente) VALUES('2017120103',  'Pedro Henrique Duarte', '10-12-2001', '12-12-2019', '2015.1');
+
+INSERT INTO Aluno(Matricula, Nome, DataNascimento, AnoConclusao, TurmaReferente) VALUES ('2017120104', 'Laura Perreira ', '10-05-2000', '17-12-2019', '2015.1');
+ 
+INSERT INTO Aluno (Matricula, Nome, DataNascimento, AnoConclusao, TurmaReferente) VALUES ('2017120105', 'Joana Vitoria Brasileiro', '10-02-2001', '17-06-2020', '2017.2');
--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -1,58 +1,58 @@
 const db = require("../config/database");
 
-//  Método responsável por criar o 'Usuário' no banco SQL Postegres:
+//  Método responsável por criar o 'Aluno' no banco SQL Postegres:
 
 exports.createUser = async (req, res) => {
-  const { nome} = req.body;
+  const { matricula, nome, datanascimento, anoconclusao, turmareferente } = req.body;
   const { rows } = await db.query(
-    "INSERT INTO usuario (nome) VALUES ($1)",
-    [nome]
+    "INSERT INTO Aluno (matricula, nome, datanascimento, anoconclusao, turmareferente) VALUES ($1, $2, $3, $4, $5 )",
+    [matricula, nome, datanascimento, anoconclusao, turmareferente]
   );
 
   res.status(201).send({
-    message: "Usuário novo Cadastrado",
+    message: "Aluno novo Cadastrado",
     body: {
       user: { nome }
     },
   });
 };
 
-//  Método responsável por lista os 'Usuários' no banco SQL Postegres:
+//  Método responsável por lista os 'Aluno' no banco SQL Postegres:
 
 exports.listarUser = async (req, res) =>{
-  const response = await db.query('SELECT * FROM usuario ORDER BY id ASC');
+  const response = await db.query('SELECT * FROM Aluno ORDER BY id ASC');
   res.status(200).send(response.rows);
 }
 
-//  Método responsável por selecionar 'Usuario' pelo 'Id' no banco SQL Postegres:
+//  Método responsável por selecionar 'Aluno' pelo 'Id' no banco SQL Postegres:
 
 exports.buscaUsuarioporId = async (req, res) => {
   const id = parseInt(req.params.id);
-  const response = await db.query('SELECT * FROM usuario WHERE id = $1', [id]);
+  const response = await db.query('SELECT * FROM Aluno WHERE id = $1', [id]);
   res.status(200).send(response.rows);
   console.log(response.rows[0].id)
 }
 
 
-//  Método responsável por atualizar o 'Usuário' pelo 'Id' no banco SQL Postegres:
+//  Método responsável por atualizar o 'Aluno' pelo 'Id' no banco SQL Postegres:
 
 exports.atualizarUserId = async (req, res) => {
   const id = parseInt(req.params.id);
   const{nome} = req.body;
 
   const response = await db.query(
-    "UPDATE usuario SET nome = $1 WHERE id = $2",
+    "UPDATE Aluno SET nome = $1 WHERE id = $2",
     [nome, id]
   );
-  res.status(200).send({message: "usuario atualizado com sucesso !"});
+  res.status(200).send({message: "Aluno atualizado com sucesso !"});
 };
 
 //  Método responsável por excluir o 'Usuário' pelo 'Id' no banco SQL Postegres:
 
 exports.deletarUserId = async (req, res) => {
   const usuarioId = parseInt(req.params.id);
-  await db.query("DELETE FROM usuario WHERE id = $1",[
+  await db.query("DELETE FROM Aluno WHERE id = $1",[
     usuarioId
   ]);
-  res.status(200).send({message: "usuario deletado com sucesso ", usuarioId});
+  res.status(200).send({message: "Aluno deletado com sucesso ", usuarioId});
 }
